06af068dbe257beb9a3a84d317a03e3086317486
彭皓呼市 <penghao@appledemacbook-pro-4.local>
2018-07-07 14:35:24 +0800

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by 彭皓 on 2018/7/7.
+//  Copyright © 2018年 彭皓. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
